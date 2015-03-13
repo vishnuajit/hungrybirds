384bbbba47705fb13f7dfd8f3e8e03160db8ab61
vishnu-ajit <heart.beats5717@gmail.com>
2015-03-13 16:22:23 +0530

    touch detected  on bird game object

--- Assets/BirdDragController.cs
+++ Assets/BirdDragController.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class BirdDragController : MonoBehaviour {
 
+	public GUIText status;
 	// Use this for initialization
 	void Start () {
-	
+	 status.text="not touched";
 	}
 	
 	// Update is called once per frame
@@ -14,9 +15,14 @@ public class BirdDragController : MonoBehaviour {
 	
 		if(Input.touchCount == 1 && Input.GetTouch(0).phase== TouchPhase.Began)
 		{
-			float x = Input.GetTouch(0).position.x;
-			Vector3 pos = new Vector3(1,0,0);
-			transform.position += pos;
+			Vector3 pos = Camera.main.ScreenToWorldPoint (Input.GetTouch(0).position);
+			RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+			if (hit != null)// && hit.collider != null) 
+			{
+			//	Debug.Log ("I'm hitting "+hit.collider.name);
+				status.text = "hit on"+hit.transform.gameObject.name;
+				}
+			
 		}
 		
 	}
