9ef821935eebcdf5be922a47d3570fdf8fc448f8
vishnu-ajit <heart.beats5717@gmail.com>
2015-03-13 09:48:44 +0530

    added a basic touch controller

---
+++ Assets/TouchController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchController : MonoBehaviour {
+
+	public GUIText statusText;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		int nTouches = Input.touchCount;
+		
+		if(nTouches > 0)
+		{
+			statusText.text="Status:"+nTouches+"touches Detected";
+			
+			for(int i = 0; i < nTouches; i++)
+			{
+				Touch touch = Input.GetTouch(i);
+				statusText.text+= "\n"+"Touch index"+touch.fingerId+"detected at position"+touch.position;
+			}
+		}
+	}
+}
