e9bf72451fdbda9123e81bbc4a1ccbe65f26abbe
vishnu-ajit <heart.beats5717@gmail.com>
2015-03-13 11:42:37 +0530

    bird moves on touch

---
+++ Assets/BirdDragController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdDragController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+		if(Input.touchCount == 1 && Input.GetTouch(0).phase== TouchPhase.Began)
+		{
+			float x = Input.GetTouch(0).position.x;
+			Vector3 pos = new Vector3(1,0,0);
+			transform.position += pos;
+		}
+		
+	}
+}
--- Assets/TouchController.cs
+++ Assets/TouchController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class TouchController : MonoBehaviour {
 
 	public GUIText statusText;
+	public GameObject bird;
 	
 	// Use this for initialization
 	void Start () {
@@ -23,6 +24,23 @@ public class TouchController : MonoBehaviour {
 			{
 				Touch touch = Input.GetTouch(i);
 				statusText.text+= "\n"+"Touch index"+touch.fingerId+"detected at position"+touch.position;
+				
+				if(touch.phase == TouchPhase.Began)
+				{
+					Ray screenRay = Camera.main.ScreenPointToRay(touch.position);
+					RaycastHit hit;
+					
+					if(Physics.Raycast(screenRay, out hit))
+					{ 
+						
+						if(hit.collider.gameObject.name=="bird")
+						{
+							Vector3 birdpos = new Vector3(touch.position.x,touch.position.y,0);
+							bird.transform.position = birdpos;
+							statusText.text="hit on bird";
+						}
+					}
+				}
 			}
 		}
 	}
